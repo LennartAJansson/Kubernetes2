1904fd985088d08771920e27e991f5943715e9c3
lennartajansson <lennart.a.jansson@outlook.com>
2023-12-14 19:21:29 +0100

    Docker images and deploy

--- InitDeploy.ps1
+++ InitDeploy.ps1
@@ -26,23 +26,23 @@ foreach($name in @(
 	kubectl apply -k ./deploy/${name}
 
 	#Restore secret.yaml and kustomization.yaml since this script alters them temporary
-	if(Test-Path -Path ./deploy/${name}/secret.yaml)
-	{
-		git checkout ./deploy/${name}/secret.yaml
-	}
-	git checkout ./deploy/${name}/kustomization.yaml
+#	if(Test-Path -Path ./deploy/${name}/secret.yaml)
+#	{
+#		git checkout ./deploy/${name}/secret.yaml
+#	}
+#	git checkout ./deploy/${name}/kustomization.yaml
 
-	$alive = ""
-	while($alive -ne "pong")
-	{
-		"Trying to connect. Could take approx 30 seconds..."
-		Start-Sleep -Seconds 10
-		$alive = &${curl} -s "${url}/Ping"
-		"Result from &${curl} -s ${url}/Ping is: " + $alive
-	}
+#	$alive = ""
+#	while($alive -ne "pong")
+#	{
+#		"Trying to connect. Could take approx 30 seconds..."
+#		Start-Sleep -Seconds 10
+#		$alive = &${curl} -s "${url}/Ping"
+#		"Result from &${curl} -s ${url}/Ping is: " + $alive
+#	}
 
-	&${curl} -X 'POST' 'http://buildversionsapi.local:8080/buildversions/CreateProject' -H 'Content-Type: application/json' -d '{"projectName": "buildversionsapi", "major": 0, "minor": 0, "build": 0, "revision": 1, "semanticVersionText": "dev"}' 
-	&${curl} -X 'POST' 'http://buildversionsapi.local:8080/buildversions/CreateProject' -H 'Content-Type: application/json' -d '{"projectName": "buildversions", "major": 0, "minor": 0, "build": 0, "revision": 1, "semanticVersionText": "dev"}' 
-	""
-	"Done!"
+#	&${curl} -X 'POST' 'http://buildversionsapi.local:8080/buildversions/CreateProject' -H 'Content-Type: application/json' -d '{"projectName": "buildversionsapi", "major": 0, "minor": 0, "build": 0, "revision": 1, "semanticVersionText": "dev"}' 
+#	&${curl} -X 'POST' 'http://buildversionsapi.local:8080/buildversions/CreateProject' -H 'Content-Type: application/json' -d '{"projectName": "buildversions", "major": 0, "minor": 0, "build": 0, "revision": 1, "semanticVersionText": "dev"}' 
+#	""
+#	"Done!"
 }
